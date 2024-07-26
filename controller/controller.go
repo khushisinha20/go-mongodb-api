8404ff367be64d0ca0a63dc3af24e79fb59a92ec
Khushi Sinha <khushisinha4786@gmail.com>
2024-07-26 09:33:52 +0530

    Update records in MongoDB

--- controller/controller.go
+++ controller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/khushisinha20/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -31,4 +34,29 @@ func init() {
 	// collection instance
 	fmt.Println("Collection instance is ready")
 }
+
+// insert one record
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted one movie in db with id: ", inserted.InsertedID)
+}
+
+// update one record
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Modified count: ", result.ModifiedCount)
+}
+
 
